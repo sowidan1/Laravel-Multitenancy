461da647fc94c444f3732e77fd5aad3e4b39c1f4
osama30 <Osama@mediafactoryworld.com>
2025-08-17 11:17:28 +0300

    return current tenant instead of welcome view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return \Spatie\Multitenancy\Models\Tenant::current(); // This will return the current tenant
 });
