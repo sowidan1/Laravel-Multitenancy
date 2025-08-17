c07fe070622e2636247d566dc67ca4ca8ba569a5
osama30 <Osama@mediafactoryworld.com>
2025-08-17 13:02:56 +0300

    add route to return the first user from the database

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return \Spatie\Multitenancy\Models\Tenant::current(); // This will return the current tenant
 });
+Route::get('/first-user', function () {
+    return \App\Models\User::first(); // This will return the first user in the database
+});
